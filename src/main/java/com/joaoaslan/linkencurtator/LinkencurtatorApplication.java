3d32c5fdb9cbdfc3343750aae2a0be5f98ab1fe5
JoaoAslan <contato.joaoaslan@gmail.com>
2023-02-04 19:15:06 -0300

    Project created

---
+++ src/main/java/com/joaoaslan/linkencurtator/LinkencurtatorApplication.java
@@ -0,0 +1,12 @@
+package com.joaoaslan.linkencurtator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkencurtatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkencurtatorApplication.class, args);
+	}
+}
